4cfc5491f7f13ada3b51aada922c6958184a7b01
Maxim Tereshchenko <maksimt@backbase.com>
2022-09-22 12:05:17 +0200

    refactor ImplementStackUsingQueues

--- src/main/kotlin/com/leetcode/ImplementStackUsingQueues.kt
+++ src/main/kotlin/com/leetcode/ImplementStackUsingQueues.kt
@@ -18,35 +18,35 @@ class ImplementStackUsingQueues {
         }
         return outputs
     }
+}
 
-    class MyStack {
+class MyStack {
 
-        private val queue: Deque<Int> = LinkedList()
+    private val queue: Deque<Int> = LinkedList()
 
-        fun push(x: Int) {
-            queue.add(x)
-        }
-
-        fun pop(): Int {
-            return queue.pollLast()
-        }
-
-        fun top(): Int {
-            return queue.peekLast()
-        }
-
-        fun empty(): Boolean {
-            return queue.isEmpty()
-        }
-
+    fun push(x: Int) {
+        queue.add(x)
     }
 
-    /**
-     * Your MyStack object will be instantiated and called as such:
-     * var obj = MyStack()
-     * obj.push(x)
-     * var param_2 = obj.pop()
-     * var param_3 = obj.top()
-     * var param_4 = obj.empty()
-     */
+    fun pop(): Int {
+        return queue.pollLast()
+    }
+
+    fun top(): Int {
+        return queue.peekLast()
+    }
+
+    fun empty(): Boolean {
+        return queue.isEmpty()
+    }
+
 }
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
